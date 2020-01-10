7761404aaa449e6fc375fdce340b2c6eae8189e5
GarKat <46765920+garkat@users.noreply.github.com>
2020-01-10 14:38:42 -0800

    Cypress

--- src/shared/views/app/app.test.js
+++ src/shared/views/app/app.test.js
@@ -1,9 +1,22 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 
 import App from './app.react';
+import NotFound from '../not-found';
 
 describe('App component', () => {
+  it('Invalid path renders "Not Found" page', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/random']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(wrapper.find(NotFound)).toHaveLength(1);
+
+    wrapper.unmount();
+  });
+
   it('Renders correctly', () => {
     const wrapper = shallow(<App />);
 
