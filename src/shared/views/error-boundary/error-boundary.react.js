1f707efe93f8aa492e130a2b14c7ff0de37250c8
GarKat <46765920+garkat@users.noreply.github.com>
2020-01-10 16:20:36 -0800

    Error boundary

---
+++ src/shared/views/error-boundary/error-boundary.react.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // TODO: use a logger such as winston, and remove the following console log statement.
+    // eslint-disable-next-line no-console
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+
+    if (hasError) {
+      return <h1>Something went wrong. Please refresh the page, and try again.</h1>;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
