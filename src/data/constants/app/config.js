ddcd4ecc1c315ee0cd73f6a2844076db1bff86a3
GarKat <46765920+garkat@users.noreply.github.com>
2020-02-21 16:45:39 -0800

    Progressive web app

--- src/data/constants/app/config.js
+++ src/data/constants/app/config.js
@@ -1,3 +1,12 @@
+const APP_GENERAL_INFO = {
+  backgroundColor: '#0c77f8',
+  description:
+    'React ecosystem boilerplate. The simplest, easiest way to build a world class, production ready app.',
+  name: 'React ecosystem boilerplate',
+  shortName: 'REB',
+  themeColor: '#0c77f8',
+};
+
 const PORTS = {
   dev: 5005,
   prod: 5014,
@@ -5,5 +14,6 @@ const PORTS = {
 };
 
 module.exports = {
+  APP_GENERAL_INFO,
   PORTS,
 };
--- src/shared/views/header/header.react.js
+++ src/shared/views/header/header.react.js
@@ -5,16 +5,46 @@ import { NavLink } from 'react-router-dom';
 
 import headerStyle from './header.scss';
 
-import { LOGO } from '../../../data/views-schema/header';
+import LOGO from '../../../data/views-schema/header';
 
 function Header(): React.Element<'header'> {
+  const logoImage = typeof LOGO.img !== 'undefined' && LOGO.img;
+
+  let logoElement;
+
+  if (logoImage !== false) {
+    if (typeof logoImage.srcSet !== 'undefined') {
+      const srcSetKeysArray = Object.keys(logoImage.srcSet);
+      let srcSet = '';
+
+      // Go through each srcSet entry, and add it to srcSet variable.
+      srcSetKeysArray.forEach((resolutionScaleFactor, index) => {
+        const srcSetPath = logoImage.srcSet[resolutionScaleFactor];
+
+        // In "img/logo-1x.png 1x" format
+        srcSet += `${srcSetPath} ${resolutionScaleFactor}`;
+
+        // Add ', ' to each srcSet entry except the last one.
+        if (index !== srcSetKeysArray.length - 1) {
+          srcSet += ', ';
+        }
+      });
+
+      logoElement = <img alt={logoImage.alt} src={logoImage.src} srcSet={srcSet} />;
+    } else {
+      logoElement = <img alt={logoImage.alt} src={logoImage.src} />;
+    }
+  } else {
+    logoElement = LOGO.name;
+  }
+
   return (
     <header
       className={`display-flex flex-items-align-center padding-all-15 ${headerStyle['header']}`}
     >
       <h1>
         <NavLink className={`width-100 ${headerStyle['logo']}`} to={LOGO.linkToLocation}>
-          {LOGO.name}
+          {logoElement}
         </NavLink>
       </h1>
     </header>
